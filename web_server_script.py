003f1174d4b59dad3136037bf95d9e6207712cc0
rajvardhanshinde <shinderajvardhan2051@gmail.com>
2025-04-12 10:01:10 +0530

    Added web server and Terraform scripts

---
+++ web_server_script.py
@@ -0,0 +1,24 @@
+import boto3
+
+def list_buckets():
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    print("Buckets in your AWS account:")
+    for bucket in response['Buckets']:
+        print(f"- {bucket['Name']}")
+
+def count_objects(bucket_name):
+    s3 = boto3.client('s3')
+    paginator = s3.get_paginator('list_objects_v2')
+    total_objects = 0
+    for page in paginator.paginate(Bucket=bucket_name):
+        if 'Contents' in page:
+            total_objects += len(page['Contents'])
+    print(f"Total number of objects in bucket '{bucket_name}': {total_objects}")
+
+if __name__ == "__main__":
+    print("Listing all buckets:")
+    list_buckets()
+    print("\nCounting objects in a specific bucket:")
+    bucket_name = input("Enter the bucket name: ")
+    count_objects(bucket_name)
